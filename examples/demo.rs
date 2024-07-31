ba38faaf14cdce6615b0540bdc7917c4a62a7e50
kindywu <kindywu@outlook.com>
2024-07-31 18:09:23 +0800

    commit

--- examples/demo.rs
+++ examples/demo.rs
@@ -1,58 +1,25 @@
-use winnow::ascii::multispace0;
-
-use winnow::combinator::opt;
 use winnow::{
-    combinator::{delimited, separated, seq},
-    token::take_until,
+    ascii::{dec_int, float},
+    combinator::alt,
     PResult, Parser,
 };
 
-#[macro_export]
-macro_rules! sep_with_space {
-    ($delimiter:expr) => {
-        seq!(multispace0, $delimiter, multispace0)
-    };
-    ($delimiter1:expr, $($delimiter:expr),+) => {
-        seq!(
-            multispace0,
-            opt($delimiter1),
-            multispace0,
-            sep_with_space!(@inner $($delimiter),+)
-        )
-    };
-    (@inner $delimiter:expr) => {
-        seq!($delimiter, multispace0)
-    };
-    (@inner $delimiter1:expr, $($delimiter:expr),+) => {
-        seq!(
-            opt($delimiter1),
-            multispace0,
-            sep_with_space!(@inner $($delimiter),+)
-        )
-    };
-}
-
 fn main() {
-    let input = r#" ["kindy", "abc", "" ,  ] "#;
-    let input = &mut (&*input);
+    let inputs = vec![r#"19.9"#, r#"-199"#, r#"19.9"#, r#"-19.9"#];
 
-    let sep1 = sep_with_space!("[");
-    // let sep2 = seq!(multispace0, opt(","), multispace0, "]", multispace0);
-    let sep2 = sep_with_space!(",", "]");
-    let sep_comma = sep_with_space!(",");
-
-    let parse_value = separated(0.., parse_string, sep_comma);
-
-    // let arr: Vec<String> = parse_value.parse_next(input).unwrap();
-
-    let arr: Vec<String> = delimited(sep1, parse_value, sep2)
-        .parse_next(input)
-        .unwrap();
-
-    println!("{:?}", arr);
+    for input in inputs {
+        let input = &mut (&*input);
+        let ret = parse_number(input);
+        println!("{:?}", ret);
+    }
 }
 
-fn parse_string(input: &mut &str) -> PResult<String> {
-    let str = delimited('"', take_until(0.., '"'), '"').parse_next(input)?;
-    Ok(str.to_owned())
+fn parse_number(input: &mut &str) -> PResult<Num> {
+    alt((float.map(Num::Float), dec_int.map(Num::Int))).parse_next(input)
+}
+
+#[derive(Debug, Clone, PartialEq)]
+enum Num {
+    Float(f64),
+    Int(i64),
 }
